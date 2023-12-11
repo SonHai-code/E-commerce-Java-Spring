b7e5469d049d16f6ba1b951f84568126a7abb648
Son Hai <haisp2002@gmail.com>
2023-12-11 12:43:16 +0700

    Build JwtBuilder file - Parse the token with JWT Validator

--- src/main/java/com/sonhai/config/AppConfig.java
+++ src/main/java/com/sonhai/config/AppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -20,7 +21,7 @@ public class AppConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeHttpRequests(Authoried -> Authoried.requestMatchers("/api/**").authenticated().anyRequest().permitAll())
-                .addFilterBefore(null, null).csrf().disable()
+                .addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class).csrf().disable()
                 .cors().configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
@@ -46,6 +47,6 @@ public class AppConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder()
+        return new BCryptPasswordEncoder();
     }
 }
