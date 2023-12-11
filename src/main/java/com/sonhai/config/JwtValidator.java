4398d32db3e0961cdcd04dc3cc6e0c38b2a52a88
Son Hai <haisp2002@gmail.com>
2023-12-11 13:23:34 +0700

    Create JwtProvider - Jwt Generator

--- src/main/java/com/sonhai/config/JwtValidator.java
+++ src/main/java/com/sonhai/config/JwtValidator.java
@@ -19,22 +19,38 @@ import javax.crypto.SecretKey;
 import java.io.IOException;
 import java.util.List;
 
+/* Validate the JWT (Json Web Token in incoming Http requests) */
 public class JwtValidator extends OncePerRequestFilter {
-
+    /*
+    Filter and get executed for each Http requests
+    Perform JWT validation and sets the authentication.
+    **/
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        /* Get Jwt from Https Header */
         String jwt = request.getHeader(JwtConstant.JWT_HEADER);
 
         if (jwt != null) {
-            // Bearer
+            /* Remove the title substring from jwt */
             jwt = jwt.substring(7);
             try {
+                /*
+                    Generates a secret key from the constant JwtConstant.SECRET_KEY
+                    using the HMAC SHA algorithm.
+                **/
                 SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
 
-                // Convert Jwt (Json Web Token) -> Jws (Json Web Signature)
+                /*
+                    Parse the JWT,
+                    Verifies its signatures using the key - "key",
+                    Extract the claim (payload) from jwt.
+                **/
                 Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
 
+                /*
+                    Get the information and authentication of User from JWT
+                **/
                 String email = String.valueOf(claims.get("email"));
 
                 String authorities = String.valueOf(claims.get("authorities"));
@@ -49,7 +65,7 @@ public class JwtValidator extends OncePerRequestFilter {
                 throw new BadCredentialsException("Invalid Token From JWT Validator");
             }
         }
-
+        /* Allow the requests to proceed the next filter of the chain */
         filterChain.doFilter(request, response);
     }
 }
