645e9a286361d5ca8c57c94e49f4cf18b180f690
Son Hai <haisp2002@gmail.com>
2023-12-12 13:18:58 +0700

    Sign up - Create new user handler with authentication.

--- src/main/java/com/sonhai/controller/AuthController.java
+++ src/main/java/com/sonhai/controller/AuthController.java
@@ -1,9 +1,69 @@
 package com.sonhai.controller;
 
+import com.sonhai.config.JwtProvider;
+import com.sonhai.exception.UserException;
+import com.sonhai.models.User;
+import com.sonhai.repository.UserRepository;
+import com.sonhai.response.AuthResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+    private UserRepository userRepository;
+    private JwtProvider jwtProvider;
+
+    private PasswordEncoder passwordEncoder;
+
+    public AuthController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws UserException {
+        String email = user.getEmail();
+        String password = user.getPassword();
+        String firstName = user.getFirstName();
+        String lastName = user.getLastName();
+
+        /* Check whether the email has been used */
+        User isEmailExist = userRepository.findByEmail(email);
+
+        if (isEmailExist != null) {
+            throw new UserException("The email has already used by another account.");
+        }
+
+        /* Create new User */
+        User createdUser = new User();
+        createdUser.setEmail(email);
+        /* Encode the password before saving to the database */
+        createdUser.setPassword(passwordEncoder.encode(password));
+        createdUser.setFirstName(firstName);
+        createdUser.setLastName(lastName);
+
+        /* Save the user to database */
+        User savedUser = userRepository.save(createdUser);
+
+        /* Setting authentication through user email and password */
+        Authentication authentication = new UsernamePasswordAuthenticationToken(savedUser.getEmail(), savedUser.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        /* Generate the token */
+        String token = jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse(token, "Signup Successfully!");
+
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<AuthResponse>
 }
