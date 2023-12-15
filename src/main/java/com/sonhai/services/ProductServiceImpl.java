ef1147d71f94f5f71d1f24157b9fa17eca6c08c2
Son Hai <haisp2002@gmail.com>
2023-12-15 14:28:39 +0700

    Build Product Service with some functions: createProduct, deleteProduct, updateProduct, findProductById, and findProductByCategory.

--- src/main/java/com/sonhai/services/ProductServiceImpl.java
+++ src/main/java/com/sonhai/services/ProductServiceImpl.java
@@ -7,11 +7,18 @@ import com.sonhai.repository.CategoryRepository;
 import com.sonhai.repository.ProductRepository;
 import com.sonhai.request.CreateProductRequest;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.condition.ProducesRequestCondition;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -74,26 +81,74 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public String deleteProduct(Long productId) throws ProductException {
-        return null;
+        Product product = findProductById(productId);
+        product.getSizes().clear();
+        productRepository.delete(product);
+        return "The product has been deleted successfully!";
     }
 
     @Override
-    public Product updateProduct(Long productId, Product product) throws ProductException {
-        return null;
+    public Product updateProduct(Long productId, Product req) throws ProductException {
+        Product product = findProductById(productId);
+
+        if(req.getQuantity() != 0){
+            product.setQuantity(req.getQuantity());
+        }
+        return productRepository.save(product);
     }
 
     @Override
     public Product findProductById(Long id) throws ProductException {
-        return null;
+        Optional<Product> opt = productRepository.findById(id);
+
+        if(opt.isPresent()) {
+            opt.get(); /* Empty return false, else return true */
+        }
+        throw new ProductException("Product not found with the id - " + id);
     }
 
     @Override
     public List<Product> findProductByCategory(String category) {
+
         return null;
     }
 
+    /**
+     * Get all Products with Pageable.
+     * */
     @Override
-    public Page<Product> getAllProducts(String category, List<String> colors, List<String> sizes, Integer minPrice, Integer maxPrice, Integer minDiscount, String sort, String stock, Integer pageNumber, Integer pageSize) {
-        return null;
+    public Page<Product> getAllProducts(String category, List<String> colors, List<String> sizes,
+                                        Integer minPrice, Integer maxPrice,
+                                        Integer minDiscount, String sort, String stock,
+                                        Integer pageNumber, Integer pageSize) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+
+        List<Product> products = productRepository.filterProducts(category, minPrice,maxPrice, minDiscount, sort);
+
+        /* Find the products with color filter */
+        if (!colors.isEmpty()){
+            products = products.stream().filter(product -> colors.stream().anyMatch(color->color.equalsIgnoreCase(product.getColor())))
+                    .collect(Collectors.toList());
+        }
+
+        if (stock != null) {
+            if(stock.equals("in_stock")){
+                products = products.stream().filter(product -> product.getQuantity() > 0).collect(Collectors.toList());
+            }
+
+            if (stock.equals("out_of_stock")) {
+                products = products.stream().filter(product -> product.getQuantity() < 1).collect(Collectors.toList());
+            }
+        }
+
+        /* Creating starting Index */
+        int startIndex = (int) pageable.getOffset();
+        int endIndex = Math.min(startIndex + pageable.getPageSize(),products.size());
+
+        List<Product> pageContent = products.subList(startIndex, endIndex);
+
+        Page<Product> filteredProducts = new PageImpl<>(pageContent, pageable, products.size());
+
+        return filteredProducts;
     }
 }
